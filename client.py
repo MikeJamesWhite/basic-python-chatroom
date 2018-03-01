cfa87578ea898028bf0448c3dc17c60f8db484d0
Michael James White <mike.james.white@icloud.com>
2018-03-01 22:32:12 +0200

    v0.03 release. A primitive and buggy CLI has been implemented. Once this interface is fixed, the groundwork for most core features will be complete. Better messaging on the server side to help understand what is happening will be implemented. Launch options to speed up the introductory input will also be added.

--- client.py
+++ client.py
@@ -4,38 +4,82 @@
 # A client which can connect to a server and receive responses until an exit
 # signal is sent. 
 #
-# v0.02
+# v0.03
 
 import socket
 import select
+import curses
 from threading import Thread
 
-name = "" # client name
+# curses init
+curses.LINES = 0
+curses.COLS = 0
+stdscr = curses.initscr()
+stdscr.keypad(True)
+curses.echo()
+begin_x = 0; begin_y = 2
+height = curses.LINES - 3
+width = curses.COLS - 1
+titleWin = curses.newwin(1, width, 0, 0)
+titleWin.addstr("basic-python-chatroom client v0.03 - by Mike White")
+titleWin.refresh()
+chatWin = curses.newwin(height, width, begin_y, begin_x)
+typeWin = curses.newwin(1, width, curses.LINES - 1, 0)
+
+# default variables
 PORT = 5090
 HOST = "mike-zenbuntu"
-receiving = True
 
 def broadcast_receiver(serverSocket):
-    while (receiving):
-        serverSocket.settimeout(2)
+    while (True):
+        serverSocket.settimeout(4)
         try:
-            print(serverSocket.recv(1024).decode())
+            chatOutput((serverSocket.recv(1024).decode()))
         except:
             return
 
-def main():
-    name = input("Enter your alias: ")
+def onExit():
+    stdscr.keypad(False)
+    curses.endwin()
 
-    serverSocket = socket.socket()
-    serverSocket.connect((HOST, PORT))
+
+def chatOutput(outStr):
+    chatWin.addstr(outStr + "\n")
+    #chatWin.clrtoeol()
+    chatWin.refresh()
+
+def userInput(inStr):
+    typeWin.addstr(inStr)
+    typeWin.refresh()
+    s = typeWin.getstr().decode()
+    typeWin.erase()
+    typeWin.refresh()
+    return s
+
+def main():
+    global HOST, PORT
+    alias = userInput("Enter your alias: ")
+    connected = False
+    while (not connected):
+        userin = userInput("Custom host and port? (y/n): ")
+        if (userin == 'y'):
+            HOST = userInput("Enter the host: ")
+            PORT = int(userInput("Enter the port number: "))
+        serverSocket = socket.socket()
+        try:
+            serverSocket.connect((HOST, PORT))
+            connected = True
+        except:
+            typeWin.addstr("Error: connection failed. ")
+
     receiverThread = Thread(target = broadcast_receiver, args = (serverSocket, ))
     receiverThread.start()
     userin = ""
     while (userin != "exit"):
-        userin = input(name + "> ")
+        userin = userInput(alias + "> ")
         serverSocket.send(userin.encode())
+    onExit()
     serverSocket.close()
-    receiving = False
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 # basic-python-chatroom: server.py
 # by Michael White (MikeJamesWhite on Github)
 #
-# A server which allows a client to connect, receives messages and sends acknowledgement
-# back to the client.
+# A server app which allows clients to connect, receives messages and rebroadcasts those
+# messages to all connected clients.
 #
-# v0.02
+# v0.03
 
 import socket
 from threading import Thread
@@ -23,6 +23,8 @@ def broadcast_to_clients(message):
 def listen_to(client):
     while True:
         message = client.recv(1024)
+        print("Server> received message.")
+        print("Server>", message.decode())
         if (message.decode() == "exit"):
             print("Server> Client disconnected.")
             clients.remove(client)
@@ -34,14 +36,18 @@ def accept_clients(serverSocket):
     print("Server> Accepting connections...")
     while True:
         client, addr = serverSocket.accept()
+        clients.append(client)
         thread = Thread(target = listen_to, args = (client, ))
         thread.start()
-        clients.append(client)
         print("Server> Accepted new client from", addr)
 
 def main():
     print('\033[H\033[J')
-    print("basic-python-chatroom: server.py v0.01\n")
+    print("basic-python-chatroom: server.py v0.03\n")
+    global PORT
+    userin = input("Custom port? (y/n) ")
+    if (userin == 'y'):
+        PORT = int(input("Enter port number: "))
     serverSocket = socket.socket()
     host = socket.gethostname()
     serverSocket.bind((host, PORT))
