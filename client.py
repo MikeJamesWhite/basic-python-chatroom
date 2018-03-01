8eb4b2a85fce1e125e27fe32f1e8eaeb87e40bff
Michael James White <mike.james.white@icloud.com>
2018-03-01 20:36:31 +0200

    v0.02 release. Server now capable of accepting multiple clients and rebroadcasting messages to the entire group of clients. UI remains an issue which will be addressed in v0.03 and beyond.

--- client.py
+++ client.py
@@ -1,16 +1,41 @@
+# basic-python-chatroom: client.py
+# by Michael White (MikeJamesWhite on Github)
+#
+# A client which can connect to a server and receive responses until an exit
+# signal is sent. 
+#
+# v0.01
+
 import socket
+import select
+from threading import Thread
+
+name = "" # client name
+PORT = 5090
+HOST = "mike-zenbuntu"
+receiving = True
+
+def broadcast_receiver(serverSocket):
+    while (receiving):
+        serverSocket.settimeout(2)
+        try:
+            print(serverSocket.recv(1024).decode())
+        except:
+            return
 
 def main():
-    s = socket.socket()
-    s.connect(("mike-zenbuntu", 5090))
-    print(s.recv(1024).decode())
-    userin = input("Enter a message: ")
-    s.send(userin.encode())
+    name = input("Enter your alias: ")
+
+    serverSocket = socket.socket()
+    serverSocket.connect((HOST, PORT))
+    receiverThread = Thread(target = broadcast_receiver, args = (serverSocket, ))
+    receiverThread.start()
+    userin = ""
     while (userin != "exit"):
-        print (s.recv(1024).decode())        
-        userin = input("Enter a message: ")
-        s.send(userin.encode())
-    s.close()
+        userin = input(name + "> ")
+        serverSocket.send(userin.encode())
+    serverSocket.close()
+    receiving = False
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -1,24 +1,54 @@
+# basic-python-chatroom: server.py
+# by Michael White (MikeJamesWhite on Github)
+#
+# A server which allows a client to connect, receives messages and sends acknowledgement
+# back to the client.
+#
+# v0.02
+
 import socket
+from threading import Thread
+
+clients = [] # list of all current clients
+PORT = 5090
+
+def broadcast_to_clients(message):
+    for client in clients:
+        try:
+            client.send(message)
+        except:
+            print("Server> Client disconnected.")
+            clients.remove(client)
+
+def listen_to(client):
+    while True:
+        message = client.recv(1024)
+        if (message.decode() == "exit"):
+            print("Server> Client disconnected.")
+            clients.remove(client)
+            return
+        thread = Thread(target = broadcast_to_clients, args = (message, ))
+        thread.start()
+
+def accept_clients(serverSocket):
+    print("Server> Accepting connections...")
+    while True:
+        client, addr = serverSocket.accept()
+        thread = Thread(target = listen_to, args = (client, ))
+        thread.start()
+        clients.append(client)
+        print("Server> Accepted new client from", addr)
 
 def main():
-    s = socket.socket()
+    print('\033[H\033[J')
+    print("basic-python-chatroom: server.py v0.01\n")
+    serverSocket = socket.socket()
     host = socket.gethostname()
-    soc = 5090
-    s.bind((host, soc))
-    print("Server: Bound to address:",host,soc)
-    s.listen()
-    print("Server: Listening for TCP connections!")
-    while(True):
-        print("Server: Waiting for a connection...")
-        client, addr = s.accept()
-        print("Server: Got connection from", addr)
-        client.send("Server: Welcome to the room!".encode())
-        message = client.recv(1024).decode()
-        while (message != "exit"):
-            print("Client: ", message)
-            client.send('Server: Got your message!'.encode())
-            message = client.recv(1024).decode()
-        print("Server: Client closed connection.")
+    serverSocket.bind((host, PORT))
+    print("Server> Bound to address '", host, ": ", PORT, sep ='', end ="'\n")
+    serverSocket.listen()
+    print("Server> Initialised TCP listener.")
+    accept_clients(serverSocket)
 
 if __name__ == '__main__':
     main()
